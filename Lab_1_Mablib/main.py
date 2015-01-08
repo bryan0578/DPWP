462e594630c0a08b6242f587c2118b792db21655
bryan0578 <bryan.cash78@gmail.com>
2015-01-08 15:41:57 -0500

    Setting up variables for madlib

---
+++ Lab_1_Mablib/main.py
@@ -0,0 +1,18 @@
+print "Madlibs!!!"
+name = raw_input("Enter your name: ")
+
+print "Hello " + name + ", welcome to Madlibs."
+print "To play Madlibs enter your answers to the following prompts and a funny story will be created. Ready....."
+story = raw_input("Are you a jedi or are you a sith: ")
+side = dict()
+side = {"jedi":"Master","sith":"Darth"}
+sillyWord1 = raw_input("Enter a silly word: ")
+adjective1 = raw_input("Enter an adjective: ")
+noun1 = raw_input("Enter a noun: ")
+sillyWord2 = raw_input("Enter a second silly word: ")
+numberValue1 = raw_input("Enter a number greater than 0 but less than 100: ")
+sillyWord3 = raw_input("Enter a third silly word: ")
+numberValue2 = raw_input("Enter another number greater than 0 but less than 1000: ")
+noun2 = raw_input("Enter another noun: ")
+adjective2 = raw_input("Enter another adjective: ")
+action = raw_input("Finally enter an action: ")
