731f80eee7b3e66796c8ce1b80b7fd2452878b3b
bryan0578 <bryan.cash78@gmail.com>
2015-01-09 01:58:10 -0500

    added comments to explain code so far

--- Lab_1_Mablib/main.py
+++ Lab_1_Mablib/main.py
@@ -1,8 +1,20 @@
+'''
+Bryan Cash
+Lab 1 Madlibs
+DPWP
+'''
+#introduction to program with prompt for welcome message
+
 print "Madlibs!!!"
 name = raw_input("Enter your name: ")
 
+#Welcome message
+
 print "Hello " + name + ", welcome to Madlibs."
 print "To play Madlibs enter your answers to the following prompts and a funny story will be created. Ready....."
+
+#Set up variables for user input, dictionary and an array for use in the madlib
+
 story = raw_input("Are you a jedi or are you a sith: ")
 side = dict()
 side = {"jedi":"Master","sith":"Darth"}
@@ -18,10 +30,14 @@ adjective2 = raw_input("Enter another adjective: ")
 action = raw_input("Finally enter an action: ")
 people = ["Han","Chewy","Leia","C3P0"]
 
-
+'''
+Funtion that takes both raw_input integers and calculates the nuber of troops used in the madlib.
+Used both logical operators and mathmematical operators
+Used one conditional statement
+'''
 
 def calcTroops(numberTroops,numberNoun):
-	if numberTroops < 50:
+	if numberTroops < 50 and numberNoun < 500:
 		troops = numberTroops*numberNoun
 		return troops
 	else:
@@ -29,4 +45,5 @@ def calcTroops(numberTroops,numberNoun):
 		return troops
 
 troop = calcTroops(numberTroops,numberNoun)
-print troop
+
+
