d1bf8cc28a45c79d5a5ee204739ccea1a322152f
bryan0578 <bryan.cash78@gmail.com>
2015-01-15 23:13:40 -0500

    added class but is not working
    I’m trying to get the input into the Employee class and write it
    through the class but I cannot figure out what I am doing wrong.

--- simple/main.py
+++ simple/main.py
@@ -3,6 +3,8 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+    	
+
     	page_head = '''<!DOCTYPE HTML>
 <html>
 	<head>
@@ -11,27 +13,32 @@ class MainHandler(webapp2.RequestHandler):
 	<body>
 		<h1>Employee Contact Database</h1>'''
 
-	page_body = '''<form method="GET">
-				<label>First Name: </label><input type="text" name="f_name" />
-				<label>Last Name </label><input type="text" name="l_name" />
-				<label>Email: </label><input type="text" name="email" />
-				<label>Phone: </label><input type="text" name="phone" />
-				<p>Preferred Contact Method: </p>
-					<label>Phone</label>
-						<input type="radio" name="contact" value="phone_number" checked/>
-					<label>Email</label>
-						<input type="radio" name="contact" value="email_address" />
-			<label>Reason for Contact</label>
-				<select name="reason">
-					<option value="pay">Pay Status</option>
-					<option value="work">Request for Work</option>
-					<option value="fired">Fired</option>
-				</select>
-			<input type="submit" value="Submit" />'''
+	page_body = '''<h2>Add Employee Contact</h2>
+			<div id="form_box">
+				<form method="GET">
+					<label>First Name: </label><input type="text" name="f_name" /><br />
+					<label>Last Name </label><input type="text" name="l_name" /><br />
+					<label>Email: </label><input type="text" name="email" /><br />
+					<label>Phone: </label><input type="tel" name="phone" /><br />
+					<p>Preferred Contact Method: </p>
+						<label>Phone</label>
+							<input type="radio" name="contact" value="phone" checked/>
+						<label>Email</label>
+							<input type="radio" name="contact" value="email" /><br />
+					<label>Reason for Contact: </label><br />
+					<select name="reason">
+						<option value="Pay Status">Pay Status</option>
+						<option value="Request for Work">Request for Work</option>
+						<option value="Fired">Fired</option>
+					</select><br />
+				<input type="submit" value="Submit" />
+			</div>'''
 
 	page_close = '''</form>
 	</body>
 </html>'''
+
+
 	if self.request.GET:
 		#stores info we got from the formm
 		f_name = self.request.GET['f_name']
@@ -41,10 +48,20 @@ class MainHandler(webapp2.RequestHandler):
 		contact = self.request.GET['contact']
 		reason = self.request.GET['reason']
 
-		self.response.write(page_head + page_body + page_close)
+		self.response.write(page_head + str(Employee()) + page_close)
 			
 	else:
 		self.response.write(page_head + page_body + page_close) #PRINT
+
+
+class Employee(object):
+	def __init__(self):
+		self.f_name = ""
+		self.l_name = ""
+		self.email = ""
+		self.phone = ""
+		self.contact = ""
+		self.reason = ""
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
