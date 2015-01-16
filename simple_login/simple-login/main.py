6a5f6406ef448c3fa68544c25bb2f46cceae0703
bryan0578 <bryan.cash78@gmail.com>
2015-01-15 20:28:39 -0500

    Started with a form and broke it into pieces
    Here is the start of my simple login assignment. I started with the
    main.py file included with the google launcher and added my html for my
    form and broke it up between page_head, page_body and page_close so
    that I can write the write the form when I want to and leave it out
    when I don’t want to write the form. I added six elements to the form:
    first name, last name, email, phone, preferred contact method as a
    radio button and finally a reason for contact as a select method.

---
+++ simple_login/simple-login/main.py
@@ -0,0 +1,60 @@
+'''
+Bryan Cash
+1/15/2015
+DPW
+Simple Login
+'''
+
+import webapp2 #use the webapp2 library
+
+class MainHandler(webapp2.RequestHandler):#declaring a class
+    def get(self): #the function that starts everything.
+    	page_head = '''<!DOCTYPE HTML>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<h1>Employee Contact Database</h1>'''
+
+		page_body = '''<form method="GET">
+			<label>First Name: </lable<input type="text" name="f_name" />
+			<label>Last Name </label><input type="text" name="l_name" />
+			<label>Email: </label><input type="text" name="email" />
+			<label>Phone: </label><input type="text" name="phone" />
+			<label>Preferred Contact Method </lable>
+				<input type="radio" name="contact" value="phone" checked/>
+				<input type="radio" name="contact" value="email" />
+			<label>Reason for Contact</label>
+				<select name="reason">
+					<option value="pay">Pay Status</option>
+					<option value="work">Request for Work</option>
+					<option value="fired">Fired</option>
+				</select>
+			<input type="submit" value="Submit" />'''
+
+		page_close = '''</form>
+	</body>
+</html>'''
+		if self.request.GET:
+			#stores info we got from the formm
+			f_name = self.request.GET['f_name']
+			l_name = self.request.GET['l_name']
+			email = self.request.GET['email']
+			phone = self.request.GET['phone']
+			contact = self.request.GET['contact']
+			reason = self.request.GET['reason']
+
+			self.response.write(page_head + user + ' ' + email + page_body + page_close)
+				pass
+		else:
+			self.response.write(page_head + page_body + page_close) #PRINT
+      
+
+  
+
+
+#never touch this section
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
