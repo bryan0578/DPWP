5526d2f10fd651dc685809513fec1a05677ac5f0
bryan0578 <bryan.cash78@gmail.com>
2015-01-29 13:42:27 -0500

    work in progress
    So i have done a lot of work I renamed some variables in the data file
    and changed it so that they are accessible through an array. I have
    added some else if statements in the main handler but they still need
    work I am still trying to figure out how to use the data from the data
    object in the page without putting the data object in the page file.

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -7,11 +7,38 @@ Dynamic Site
 
 
 import webapp2
-
+from page import Page, ContentPage
+from data import Shirt
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        p = Page()
+        cp = ContentPage
+        s = Shirt()
+
+
+        if self.request.GET:
+            if self.request.GET['id'] == "victorian_woman":
+                cp.data = s.shirts[0]
+                self.response.write(cp.content_view())
+            elif self.request.GET['id'] == "metal_skull":
+                cp.data = s.shirts[1]
+                self.response.write(cp.content_view())
+            elif self.request.GET['id'] == "three_b":
+                cp.data = s.shirts[2]
+                self.response.write(cp.content_view())
+            elif self.request.GET['id'] == "f_bomb":
+                cp.data = s.shirts[3]
+                self.response.write(cp.content_view())
+            elif self.request.GET['id'] == "hoody":
+                cp.data = s.shirts[4]
+                self.response.write(cp.content_view())
+
+        else:
+            self.response.write(p.print_out())
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- dynamic-site/page.py
+++ dynamic-site/page.py
@@ -8,20 +8,48 @@ Dynamic Site
 
 class Page(object):#borrow stuff from object class
     def __init__(self):
+        self.data = TShirts()
+        self.content = ''
         self._head = '''
 <!DOCTYPE HTML>
 <html>
     <head>
-        <title></title>
+        <title>Welcome to the Web Store</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
     </head>
     <body>'''
-
         self._body = '''
-        <h1> Beneath The Sky Merch Store</h1>
+        <h1> Beneath The Sky Web Store</h1>
+        <a href="?id=victorian_woman">Victorian Woman</a><br>
+        <a href="?id=three_b">Three B's</a><br>
+        <a href="?id=metal_skull">Metal Skull</a><br>
+        <a href="?id=f_bomb">F - Bomb</a><br>
+        <a href="?id=hoody">Hoody</a><br>
+        <a href="?id=wddts">What Demons Do To Saints</a><br>
+        <a href="?id=tdtmd">The Day The Music Died</a><br>
+        <a href="?id=iml">In Loving Memory</a><br>
         '''
         self._close = '''
     </body>
 </html>'''
 
     def print_out(self):
-        return self._head + self._body + self.close
+        return self._head + self._body + self._close
+
+
+class ContentPage(Page):
+    def __init__(self):
+        super(ContentPage, self).__init__()
+
+
+class TShirts(object):
+    def __init__(self):
+        self.id = 0
+        self.name = ''
+        self.size = ''
+        self.img = ''
+        self.description = ''
+        self.price = 0
+        self.clearance = 0.50
+        self.final_price = 0
+
